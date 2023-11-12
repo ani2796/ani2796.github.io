3e1c3a3ce6f2bebce2be4a6f2225e2beda9fb2dc
Anirudh Sriram <anirudh.sriram96@gmail.com>
2023-11-12 17:46:58 -0500

    Remove unused variable

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Header from './components/Header';
 import Page from './components/Page';
 
 function App() {
-    const [pageName, setPageName] = useState('home');
+    const [pageName, _] = useState('home');
 
     return (
         <>
